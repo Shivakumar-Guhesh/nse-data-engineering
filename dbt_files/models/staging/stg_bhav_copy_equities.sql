5783baee6e8ca0cd7ca8c52a0b2e07ddad3eed43
Shivakumar G <shivakumarguhesh@gmail.com>
2024-03-25 17:47:31 +0000

    'Equity Fact'

--- dbt_files/models/staging/stg_bhav_copy_equities.sql
+++ dbt_files/models/staging/stg_bhav_copy_equities.sql
@@ -1,33 +1,40 @@
-with 
+{{ 
+  config(
+    materialized = 'view',
+    incremental_strategy = 'insert_overwrite',
+    partition_by = { 
+      'field': 'timestamp',
+      'data_type': 'timestamp',
+      'granularity': 'day' 
+    }
+  ) 
+}} 
 
-source as (
-
-    select * from {{ source('staging', 'bhav_copy_equities') }}
-
-),
-
-renamed as (
-
-    select
-        {{ dbt_utils.generate_surrogate_key(["symbol", "timestamp"]) }} as equity_id,
-        symbol,
-        series,
-        open,
-        high,
-        low,
-        close,
-        last,
-        prevclose,
-        tottrdqty,
-        tottrdval,
-        timestamp,
-        totaltrades,
-        {{ get_moving_average(50,"close","timestamp","symbol") }} as ma_50,
-        {{ get_moving_average(200,"close","timestamp","symbol") }} as ma_200,
-        {{ change_in_price() }} as change_in_close_price,
-        {{ pct_chg_price() }} as pct_change_in_close_price
-    from source
-
+with source as (
+  select *,
+    row_number() over(partition by symbol, timestamp) as rn
+  from
+    {{ source('staging', 'bhav_copy_equities') }}
 )
-
-select * from renamed
+select
+  {{ dbt_utils.generate_surrogate_key(["symbol", "timestamp"]) }} as equity_id,
+  symbol,
+  series,
+  open,
+  high,
+  low,
+  close,
+  last,
+  prevclose,
+  tottrdqty,
+  tottrdval,
+  timestamp,
+  totaltrades,
+  {{ get_moving_average(50, "close", "timestamp", "symbol") }} as ma_50,
+  {{ get_moving_average(200, "close", "timestamp", "symbol") }} as ma_200,
+  {{ change_in_price() }} as change_in_close_price,
+  {{ pct_chg_price() }} as pct_change_in_close_price
+from 
+  source
+where
+  rn = 1
